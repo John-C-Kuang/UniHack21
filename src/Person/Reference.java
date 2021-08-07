c303ac94b3f0f030c2b3bf682d8d61bb75309605
JerryGCDing <jerrydty@outlook.com>
2021-08-07 20:14:50 +0800

    Person getter update

---
+++ src/Person/Reference.java
@@ -0,0 +1,11 @@
+package Person;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Reference {
+  public static final ArrayList<Float> PR_POS_SYMPTOMS = new ArrayList<Float>(Arrays.asList(74.2f,
+      58.3f, 56.7f, 45.0f, 34.2f, 31.7f, 30.0f, 29.2f, 28.3f, 27.5f, 23.3f, 21.7f, 19.2f, 15.8f));
+  public static final ArrayList<Float> PR_NEG_SYMPTOMS = new ArrayList<Float>(Arrays.asList(65.8f,
+      60.0f, 48.3f, 43.3f, 5.8f, 39.2f, 6.7f, 25.0f, 16.7f, 31.7f, 20.0f, 21.7f, 14.2f, 21.7f));
+}
