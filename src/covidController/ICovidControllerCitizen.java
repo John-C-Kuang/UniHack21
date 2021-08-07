df1cc227df4927bf60095e7783dc8012a82d0099
jsswd888 <ShikwKevin@gmail.com>
2021-08-07 23:38:28 +0800

    added func for iCovidController interface

--- src/covidController/ICovidControllerCitizen.java
+++ src/covidController/ICovidControllerCitizen.java
@@ -1,7 +1,12 @@
 package covidController;
 
+import Person.IPerson;
+
 public interface ICovidControllerCitizen extends ICovidController {
   String queryMedicalAdvice();
 
   boolean queryTravelPermission();
+
+  String checkTravelHistoory(IPerson person);
+  
 }
--- src/covidController/ICovidControllerGovernment.java
+++ src/covidController/ICovidControllerGovernment.java
@@ -1,5 +1,16 @@
 package covidController;
 
-public interface ICovidControllerGovernment {
+import Person.IPerson;
 
+public interface ICovidControllerGovernment {
+    Long totalInfectionRate();
+
+    IPerson personTravelHistory(IPerson person);
+
+    boolean setTravelPermission(IPerson person);
+
+    void addPerson(IPerson person);
+
+    void delPerson(IPerson person);
+    
 }
